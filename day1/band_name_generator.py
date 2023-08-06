7c92359c5f772dc0bcec1651d493127c39797110
IbrahimGanaMalgwi <ibrahimganamalgwi@outlook.com>
2023-08-06 17:59:55 +0100

    update

--- day1/band_name_generator.py
+++ day1/band_name_generator.py
@@ -5,5 +5,6 @@ print("")
 city = input("What's name of the city you grew up in? \n")
 pet = input("What's your pet's name? \n")
 
-band_name = city + " " + pet
-print(band_name)
+text_string = "Your band name could be " + city + " " + pet
+# band_name = city + " " + pet
+print(text_string)
