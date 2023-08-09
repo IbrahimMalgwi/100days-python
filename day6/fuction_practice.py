06935d048f4b0193226cbaf90e65c5bc200400cc
IbrahimGanaMalgwi <ibrahimganamalgwi@outlook.com>
2023-08-09 19:46:06 +0100

    update

--- day6/fuction_practice.py
+++ day6/fuction_practice.py
@@ -1,3 +1,22 @@
 def my_function():
     print("Hello from a function")
     print("Bye")
+
+
+hello = "hello_world"
+print(len(hello))
+print("my mane")
+
+a_str = "He had the bat"
+print(a_str.find('t'))
+print(a_str.find('t', 8))
+print(a_str.find('t', a_str.find('t')+1))
+
+river = "Mississippi"
+target = input("Input a character to find: ")
+for index in range(len(river)):
+    if river[index] == target:
+        print("Letter found at index ", index)
+        break
+    else:
+        print("Letter", target, "not found in", river)
